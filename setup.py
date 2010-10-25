720d2564dc09dce47159adcb354609c51ee32a99
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-10-25 14:47:34 +0000

    Back to development: 0.4.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.4.1'
+version = '0.4.2dev'
 
 readme = open(os.path.join('src', 'grokcore', 'message', 'README.txt')).read()
 changes = open("CHANGES.txt").read()
