19ea964b0a3e9caee64da93767ab28f70fbbae56
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-17 13:42:29 +0100

    Preparing release 3.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '3.0.1.dev0'
+version = '3.0.1'
 
 readme = open(os.path.join('src', 'grokcore', 'message', 'README.txt')).read()
 changes = open("CHANGES.txt").read()
