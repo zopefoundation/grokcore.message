495211be6fbbcf14853161b434eae6131db5c1f3
Michael Howitz <icemac@gmx.net>
2025-06-18 08:18:57 +0200

    Switch to PEP 420 native namespace. (#9)
    * Bumped version for breaking release.
    
    * Replace ``pkg_resources`` namespace with PEP 420 native namespace.
    
    * Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
-version = '4.2.dev0'
+version = '5.0.dev0'
 
 with open(os.path.join('src', 'grokcore', 'message', 'README.rst')) as f:
     readme = f.read()
@@ -24,7 +23,7 @@ tests_require = [
     'zope.publisher',
     'zope.security',
     'zope.session >= 5.1',
-    'zope.testrunner',
+    'zope.testrunner >= 6.4',
 ]
 
 setup(name='grokcore.message',
@@ -36,9 +35,6 @@ setup(name='grokcore.message',
       author_email='grok-dev@zope.dev',
       url='https://github.com/zopefoundation/grokcore.message',
       license='ZPL-2.1',
-      namespace_packages=['grokcore'],
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
       include_package_data=True,
       zip_safe=False,
       python_requires='>=3.9',
